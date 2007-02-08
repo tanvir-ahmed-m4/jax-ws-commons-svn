22d4b1ebc706fbc0878e74b719e952bf9d152b2b
kohsuke <kohsuke@localhost>
2007-02-08 23:15:16 +0000

    pushing 1.1 doc

--- www/spring/sorcerer/org/springframework/web/context/ServletContextAware-usage.js
+++ www/spring/sorcerer/org/springframework/web/context/ServletContextAware-usage.js
@@ -1,2 +1,2 @@
 setClassUsage('org.springframework.web.context.ServletContextAware',
-{"this":{"kind":"package","name":"","children":[{"kind":"package","name":"org","children":[{"kind":"package","name":"jvnet","children":[{"kind":"package","name":"jax_ws_commons","children":[{"kind":"package","name":"spring","classes":[{"name":"SpringService","kind":"class","access":"public","line":40,"leaves":[40]}]}]}]}]}]}});
+{"this":{"kind":"package","name":"","children":[{"kind":"package","name":"org","children":[{"kind":"package","name":"jvnet","children":[{"kind":"package","name":"jax_ws_commons","children":[{"kind":"package","name":"spring","classes":[{"name":"SpringService","kind":"class","access":"public","line":43,"leaves":[43]}]}]}]}]}]}});
